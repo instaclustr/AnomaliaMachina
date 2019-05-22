3422f049838a9e775c01de14cf02baa0bf87062d
paul-brebner <31493691+paul-brebner@users.noreply.github.com>
2019-05-22 10:20:24 +1000

    Add files via upload
    scripts and Dockerfiles for deploying consumer and producer jar files to Docker and Kubernetes

---
+++ deploy_consumer.sh
@@ -0,0 +1,7 @@
+docker build -f Dockerfile.consumer -t consumer .
+docker tag consumer user/consumer:latest
+docker push user/consumer
+kubectl delete deployment consumer-deployment
+kubectl create -f k8_deployment.yaml
+kubectl get deployments
+kubectl get services
---
+++ deploy_producer.sh
@@ -0,0 +1,7 @@
+docker build -f Dockerfile.producer -t producer .
+docker tag producer user/producer:latest
+docker push user/producer
+kubectl delete deployment producer-deployment
+kubectl create -f k8_producer_deployment.yaml
+kubectl get deployments
+kubectl get services
